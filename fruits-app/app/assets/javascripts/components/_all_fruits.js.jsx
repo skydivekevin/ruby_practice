f60dd2d2ad377d36f3abb16220a698652257d32b
skydivekevin <skydivekevin@gmail.com>
2019-02-10 15:03:39 -0700

    Create and Read are working, delete has error; check body line 39; handleDelete is not a function; everything else working
    Signed-off-by: skydivekevin <skydivekevin@gmail.com>

---
+++ fruits-app/app/assets/javascripts/components/_all_fruits.js.jsx
@@ -0,0 +1,32 @@
+const AllFruits = (props) => {
+  var fruits = props.fruits.map((fruit) => {
+      return(
+        <div key={fruit.id}>
+         <Fruit fruit={fruit}/>
+        </div>
+      )
+    })
+  return(
+        <div>
+          {fruits}
+        </div>
+      )
+  }
+
+
+
+// const AllFruits = (props) => {
+//   var fruits = props.fruits.map((fruit) => {
+//       return(
+//         <div key={fruit.id}>
+//           <h1>{fruit.name}</h1>
+//           <p>{fruit.description}</p>
+//         </div>
+//       )
+//     })
+//   return(
+//         <div>
+//           {fruits}
+//         </div>
+//       )
+//   }
